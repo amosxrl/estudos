d443e93ef1b3994c44834cb321fdd44fc06698af
amosxrl <amosxrl@outlook.com>
2025-02-14 15:16:15 -0300

    deletar java do curso em video

--- curso-em-video/java/aula04/src/aula04/OlaMundo.java
+++
@@ -1,9 +0,0 @@
-package aula04;
-
-public class OlaMundo {
-    public static void main(String[] args) {
-        System.out.println(" ");
-        System.out.println("Olá, Mundo! ");
-        System.out.println(" ");
-    }
-}
